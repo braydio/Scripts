751547751927a143c5b4c544d5430f212efff2ff
braydio <chaffee.brayden@gmail.com>
2025-08-26 01:43:41 -0400

    cleanup-tool.s

--- device-mgmt/update-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update the first Raspberry Pi
-ssh braydenchaffee@192.168.1.85 'sudo apt-get update && sudo apt-get upgrade -y'
-echo "Raspberry Pi B-4: Updated"
-
-# Update the second Raspberry Pi
-ssh braydenchaffee@192.168.1.78 'sudo apt-get update && sudo apt-get upgrade -y'
-echo "Raspberry Pi W-0: Updated"
